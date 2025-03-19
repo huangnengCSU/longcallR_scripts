ec821bc847c7383857c8a8c88b33a73bec50a3c2
Neng Huang <neng@ds.dfci.harvard.edu>
2025-03-19 17:20:37 -0400

    set up pseudo base quality for fastq with abnormal base quality

--- set_pseudo_baseq.py
+++ set_pseudo_baseq.py
@@ -2,30 +2,20 @@ import sys
 import gzip
 from Bio import SeqIO
 
-# Check if the input file is provided
 if len(sys.argv) != 4:
-    sys.exit("Usage: python modify_fake_baseq.py input.fastq output.fastq fake_quality")
+    sys.exit("Usage: python set_pseudo_baseq.py input.fastq output.fastq base_quality_value")
 
-# Input and output file paths
 input_fastq = sys.argv[1]
 output_fastq = sys.argv[2]
 fake_quality = int(sys.argv[3])
 
-# Open input file (handle compressed files)
 if input_fastq.endswith(".gz"):
     input_handle = gzip.open(input_fastq, "rt")
 else:
     input_handle = open(input_fastq, "r")
-
-# Open output file
 output_handle = open(output_fastq, "w")
-
-# Read the FASTQ file and modify the quality scores
 with input_handle, output_handle:
     for record in SeqIO.parse(input_handle, "fastq"):
-        # Set fake quality scores
         record.letter_annotations["phred_quality"] = [fake_quality] * len(record.seq)
-        # Write the modified record to the output file
         SeqIO.write(record, output_handle, "fastq")
-
 print(f"FASTQ file with modified quality scores saved to {output_fastq}")
